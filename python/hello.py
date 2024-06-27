4befce2588e1b2962ed82b91845fc708e1e1a6ce
Amit Kumar <amitrke@gmail.com>
2024-06-27 16:13:36 -0400

    Updated APIGateway code

---
+++ python/hello.py
@@ -0,0 +1,11 @@
+import json
+
+#AWS Lambda function to say hello
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": "{'Test': 'Test'}",
+        "headers": {
+            'Content-Type': 'text/html',
+        }
+    }
